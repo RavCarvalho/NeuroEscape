f96c90fc6103518dbbf78b6d173433f25de289b7
SofiaHeisenberg <sofiaheisenberg26@gmail.com>
2025-03-14 21:35:57 -0300

    criei a cena do level2 e escadaria, tbm adicionei o tutorial

--- Scripts/menu.gd
+++ Scripts/menu.gd
@@ -17,3 +17,15 @@ func _on_button_button_up() -> void:
 
 func _on_sair_button_up() -> void:
 	get_tree().quit()  # Fecha o aplicativo
+
+
+func _on_fechar_email_button_up() -> void:
+	$email.visible = false
+
+
+func _on_tutorial_button_up() -> void:
+	$tutorial2.visible = true
+
+
+func _on_fechartutorial_button_up() -> void:
+	$tutorial2.visible = false
