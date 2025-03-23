abb07f207c0e212cb0faa0bfd8ad4ec8b3381ec2
SofiaHeisenberg <sofiaheisenberg26@gmail.com>
2025-03-23 10:44:11 -0300

    ajeitei as falas e o volume tbm

--- Scripts/menu.gd
+++ Scripts/menu.gd
@@ -4,6 +4,8 @@ extends Node2D
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
 	MusicManager.playMusic("calma")
+	
+	
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
